0b591b6b82377cb5972d7cb7a6ccf54726f144da
PAI TEl <ttay265@gmail.com>
2016-07-19 20:59:52 +0700

    Import Lab2

---
+++ Product/ProductManagement2.0/Menu.cs
@@ -0,0 +1,98 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ProductManagement2._0
+{
+    public class Menu : List<String>
+    {
+        #region Attributes & Properties 
+        private string _Title;
+        private bool _ClearPreviousTask;
+        private bool _ExitOption;
+        private string _LastTaskMessage;
+
+        public string LastTaskMessage
+        {
+            get { return _LastTaskMessage; }
+            set { _LastTaskMessage = value; }
+        }
+
+        public bool ExitOption
+        {
+            get { return _ExitOption; }
+            set { _ExitOption = value; }
+        }
+
+        public bool ClearPreviousTask
+        {
+            get { return _ClearPreviousTask; }
+            set { _ClearPreviousTask = value; }
+        }
+
+
+        public string Title
+        {
+            get { return Title; }
+            set { Title = value; }
+        }
+        #endregion
+        #region Constructor
+        public Menu()
+        {
+            this._ExitOption = false;
+        }
+
+        public Menu(string Title)
+        {
+            this._ExitOption = false;
+            this._Title = Title;
+        }
+        #endregion
+
+        public void AddLine(string line)
+        {
+            this.Add(line);
+        }
+        public int select()
+        {
+            if (_ClearPreviousTask)
+            {
+                Console.Clear();
+                this.Display();
+            }
+            try
+            {
+                int choice = int.Parse(Console.ReadLine());
+                if (choice >= 0 && choice <= this.Count)
+                {
+                    return choice;
+                }
+            }
+            catch (Exception ex)
+            {
+            }
+            return -1;
+        }
+        public void Display()
+        {
+            if (!this._Title.Any())
+            {
+                this._Title = "*";
+            }
+            Console.WriteLine("*****************{0}*****************\n", this._Title);
+            foreach (string line in this)
+            {
+                Console.WriteLine(line);
+            }
+            Console.WriteLine("*************************************\n");
+            if (!string.IsNullOrEmpty(LastTaskMessage))
+            {
+                Console.WriteLine("Message: \n{0}", LastTaskMessage);
+            }
+            Console.Write("\nSelect: ");
+        }
+    }
+}
